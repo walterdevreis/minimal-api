f9acbe44c96efe90149138f28168fa3502106422
walter dev reis <walter-reis@outlook.com>
2025-11-29 17:46:34 -0300

    Concluída a conexão do Entity Framework com o Banco de dados MySql

--- Program.cs
+++ Program.cs
@@ -1,11 +1,26 @@
+using MinimalApi.Infraestrutura.Db;
+using MinimalApi.DTOs;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
+
+
 
 app.MapGet("/", () => "Hello World!");
 
 app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) =>
 {
-    if(loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    if(loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "1234567")
     {
         return Results.Ok("Login com sucesso");
     }
